996b18cae18bdf14a46c3b6e4700884a57ed7a26
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-04-04 14:09:00 -0300

    feat: add create and findDetails event into event controller

--- src/main/java/com/zilmar/passin/controllers/EventController.java
+++ src/main/java/com/zilmar/passin/controllers/EventController.java
@@ -1,16 +1,41 @@
 package com.zilmar.passin.controllers;
 
+import com.zilmar.passin.dto.event.CreateEventRequestDto;
+import com.zilmar.passin.dto.event.EventDetailsResponseDto;
+import com.zilmar.passin.dto.event.EventIdDto;
+import com.zilmar.passin.services.EventService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/events")
+@ResponseBody
+@RequiredArgsConstructor
 public class EventController {
-    @GetMapping
-    public ResponseEntity<String> index() {
-        return ResponseEntity.ok("Success Events!");
+    private final EventService eventService;
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDetailsResponseDto> getEventDetails(@PathVariable UUID eventId) {
+        EventDetailsResponseDto details = this.eventService.getEventDetail(eventId);
+        return ResponseEntity.ok(details);
+    }
+
+    @PostMapping()
+    public ResponseEntity<EventIdDto> createEvent(
+            @RequestBody CreateEventRequestDto body,
+            UriComponentsBuilder uriComponentsBuilder
+    ) {
+        EventIdDto eventIdDto = this.eventService.createEvent(body);
+
+        var uri = uriComponentsBuilder
+                .path("/events/{eventId}")
+                .buildAndExpand(eventIdDto.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDto);
     }
 }
