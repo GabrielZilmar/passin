6a8448ed3406aabf1dd916f033eb2cd96d146af8
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-04-04 16:33:01 -0300

    feat: add findAllWithCheckInByEventId

--- src/main/java/com/zilmar/passin/repositories/AttendeeRepository.java
+++ src/main/java/com/zilmar/passin/repositories/AttendeeRepository.java
@@ -2,10 +2,17 @@ package com.zilmar.passin.repositories;
 
 import com.zilmar.passin.domain.attendees.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, UUID> {
-    public List<Attendee> findByEventId(UUID eventId);
+    public List<Attendee> findAllByEventId(UUID eventId);
+
+    @Query(value =
+            "SELECT attendees.* FROM attendees " +
+            "LEFT JOIN check_ins ON attendees.id = check_ins.attendee_id " +
+            "WHERE attendees.event_id = ?1", nativeQuery = true)
+    public List<Attendee> findAllWithCheckInByEventId(UUID eventId);
 }
