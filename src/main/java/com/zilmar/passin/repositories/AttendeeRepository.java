cd199f080ef90c9c542698881159a2030b1ac8fd
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-04-04 14:07:27 -0300

    feat: add findByEventId method

--- src/main/java/com/zilmar/passin/repositories/AttendeeRepository.java
+++ src/main/java/com/zilmar/passin/repositories/AttendeeRepository.java
@@ -3,7 +3,9 @@ package com.zilmar.passin.repositories;
 import com.zilmar.passin.domain.attendees.Attendee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, UUID> {
+    public List<Attendee> findByEventId(UUID eventId);
 }
