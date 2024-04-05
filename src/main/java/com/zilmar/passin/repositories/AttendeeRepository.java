9667e28025136b28ea6dd7f764cb34e3ad131b59
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2024-04-04 22:05:22 -0300

    feat: add findByuEventIdAndEmail to attendeeRepository

--- src/main/java/com/zilmar/passin/repositories/AttendeeRepository.java
+++ src/main/java/com/zilmar/passin/repositories/AttendeeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AttendeeRepository extends JpaRepository<Attendee, UUID> {
@@ -15,4 +16,6 @@ public interface AttendeeRepository extends JpaRepository<Attendee, UUID> {
             "LEFT JOIN check_ins ON attendees.id = check_ins.attendee_id " +
             "WHERE attendees.event_id = ?1", nativeQuery = true)
     public List<Attendee> findAllWithCheckInByEventId(UUID eventId);
+
+    public Optional<Attendee> findByEventIdAndEmail(UUID eventId, String email);
 }
